e7e391d6d3a15a74643b4e3464338ed28fbf256e
bwu <bwu@xogrp.com>
2018-05-07 17:53:18 +0800

    INIT

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view>Hello everyone</router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'comrade-electron'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div >
+        <div >
+          <div >Login</div>
+        </div>
+        <div >
+          <div >Other Documentation</div>
+          <div>
+              <label>用户名: </label> <label><input></input></label>
+            </div>
+          <div>
+              <label>密码:    </label> <label><input></input></label>
+            </div>
+        </div>
+        <div><button @click="login()">登录</button></div>
+      </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  components: {},
+  methods: {
+    async login () {
+      console.log('hello world')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
